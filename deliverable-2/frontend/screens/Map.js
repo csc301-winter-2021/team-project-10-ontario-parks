c23a0721ee24728970b5d8ee76e0af064e09d8fc
linpao <edward9292@gmail.com>
2021-03-08 06:04:41 -0500

    add skeleton code for Login.js Map.js SettingPreference.js SignUp.js

---
+++ deliverable-2/frontend/screens/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const Map = props =>{
+    return(
+        <View>
+            /* TODO */
+        </ View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
---
+++ deliverable-2/frontend/screens/SettingPreference.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const SettingPreference = props =>{
+    return(
+        <View>
+            /* TODO */
+        </ View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SettingPreference;
