2a3c23739a3c34751895023846368804726e89f3
linpao <edward9292@gmail.com>
2021-03-14 20:48:37 -0400

    update MainPage.js for routes

--- deliverable-2/frontend/screens/MainPage.js
+++ deliverable-2/frontend/screens/MainPage.js
@@ -2,15 +2,20 @@ import React from 'react';
 import {View, Button, StyleSheet} from 'react-native';
 import Header from '../components/Header';
 
-const MainPage = props =>{
+const MainPage = (props) => {
     const tytle = "Explore Ontario";
+
+    //function allow user to switch the pages when call
+    const pressHandler = (page) =>{
+        props.navigation.navigate(page);
+    }
 
     return(
         <View>
             <Header title={tytle}></Header>
-            <Button title="Setting Preference" />
-            <Button title="Map" />
-            <Button title="Start" />
+            <Button title="Setting Preference" onPress={()=>{pressHandler('SettingPreference')}}/>
+            <Button title="Map" onPress={()=>{pressHandler('Map')}}/>
+            <Button title="Start"/>
         </View>
     );
 };
