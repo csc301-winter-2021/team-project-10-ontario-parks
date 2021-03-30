226c56e2e0e90291323935a943552ab71ab6d682
linpao <edward9292@gmail.com>
2021-03-30 00:19:59 -0400

    add setting preference

--- deliverable-3/backend/server.js
+++ deliverable-3/backend/server.js
@@ -3,11 +3,12 @@ const app = express()
 app.use(express.json())
 PORT = 3000
 
-
+//read user data from ./data/user_data.json
 const fs = require('fs')
 let userjson = fs.readFileSync("./data/user_data.json", "utf-8")
 let user_data = JSON.parse(userjson)
 
+//handle login request
 app.get('/login', (req, res) => {
     const useremail = req.body.email
     const userpassword = req.body.password
@@ -24,7 +25,7 @@ app.get('/login', (req, res) => {
         res.status(404).send("email or password incorrect")
     }
 })
-
+//handle signup request
 app.post('/signup', (req, res)=>{
     const useremail = req.body.email
     let found = false
@@ -39,15 +40,33 @@ app.post('/signup', (req, res)=>{
             "name": req.body.name,
             "email": req.body.email,
             "password": req.body.password,
-            "preference": req.body.preference
+            "preference": req.body.preference||[]
         }
         user_data.push(user)
-        userjson = JSON.stringify(user_data)
+        userjson = JSON.stringify(user_data,null, 2)
         fs.writeFileSync("./data/user_data.json",userjson, "utf-8")
         res.status(201).send("success")
     }
-
-
 })
+//handle setting user preference request
+app.post('/preference', (req, res)=>{
+    const useremail = req.body.email
+    const userpreference = req.body.preference
+    let found = false
+    user_data.forEach((user)=>{
+        if(user.email === useremail){
+            found = true
+            user.preference = userpreference
+            res.status(201).send("success")
+        }
+    })
+    if(!found){
+        res.status(404).send("user doesn't exist")
+    }
+    userjson = JSON.stringify(user_data,null, 2)
+    fs.writeFileSync("./data/user_data.json",userjson, "utf-8")
+})
+
+
 
 app.listen(PORT)
